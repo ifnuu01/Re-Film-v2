0fb9bc02f3473daac9a66d39f3a90e7c3d128356
ifnuu01 <ifnuu01@gmail.com>
2025-02-09 20:51:46 +0800

    Menghubungkan template auth login dan regis dengan view yg baru

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,35 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Login</title>
+</head>
+<body class="bg-[#22252F]">
+    <div class="flex justify-content items-center h-screen">
+        <form class="bg-[#161A20] shadow-lg w-full lg:w-2/6 mx-auto py-8 px-4 lg:rounded-lg shadow-lg h-fit flex flex-col items-center" action="{{route('login')}}" method="post">
+            <h2 class="text-center animate-pulse font-bold text-3xl py-2 text-transparent bg-clip-text bg-gradient-to-r from-[#FC882F] to-[#2EBCF9]">Login Into Re Film</h2>
+            @csrf
+            <div class="w-80 py-2">
+                <label for="email" class="text-white font-bold">Email</label>
+                <input type="email" name="email" id="email" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('email')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <label for="password" class="text-white font-bold">Password</label>
+                <input type="password" name="password" id="password" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('password')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <button type="submit" class="w-full px-4 py-2 bg-gradient-to-r from-[#FC882F] to-[#2EBCF9] text-white font-bold rounded-lg">Login</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,49 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Register</title>
+</head>
+<body class="bg-[#22252F]">
+    <div class="items-center h-screen flex flex-col justify-center">
+        <h2 class="text-center animate-pulse font-bold text-3xl py-2 text-transparent bg-clip-text bg-gradient-to-r from-[#FC882F] to-[#2EBCF9]">Register Into Re Film <br>👋</h2> 
+        <form class="bg-[#22252F] shadow-2xl w-full lg:w-2/6 mx-auto py-8 px-4 lg:rounded-lg shadow-lg h-fit flex flex-col items-center" action="{{route('register')}}" method="post">
+            @csrf
+            <div class="w-80 py-2">
+                <label for="name" class="text-white font-bold">Name</label>
+                <input type="text" name="name" id="name" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('name')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <label for="email" class="text-white font-bold">Email</label>
+                <input type="email" name="email" id="email" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('email')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <label for="password" class="text-white font-bold">Password</label>
+                <input type="password" name="password" id="password" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('password')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <label for="password_confirmation" class="text-white font-bold">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="w-full px-4 py-2 bg-[#161A20] border border-[#2EBCF9] text-white rounded-lg focus:outline-none focus:border-[#FC882F]">
+                @error('password_confirmation')
+                    <span class="text-red-500 mt-2">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="w-80 py-2">
+                <button type="submit" class="w-full shadow-lg px-4 py-2 bg-gradient-to-r from-[#FC882F] to-[#2EBCF9] text-white font-bold rounded-lg">Register</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
