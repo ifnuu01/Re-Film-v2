468df5ea6f27fe3b399a0ce4161866f81e6fc2f4
ifnuu01 <ifnuu01@gmail.com>
2025-02-10 19:03:04 +0800

    memperbaiki view dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,28 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.dashboard')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+
+@section('title', 'Dashboard')
+
+
+@section('content')
+    <div class="flex flex-col gap-y-4">
+        @if (session('status'))
+            <div class="bg-green-500 text-white p-4 rounded-lg">
+                {{ session('status') }}
+            </div>
+        @endif
+        <h2 class="text-3xl text-white font-bold">Welcome to dashboard</h2>
+        <div class="flex items-center gap-x-4 bg-[#22252F] p-4 rounded-lg">
+            @if(auth()->user()->profile->photo)
+                <img src="{{ asset('storage/' . auth()->user()->profile->photo) }}" alt="avatar" class="w-20 h-20 rounded-full object-cover">
+            @else
+                <img src="https://ui-avatars.com/api/?name={{auth()->user()->name}}&background=random&color=fff" alt="avatar" class="w-20 h-20 rounded-full">
+            @endif
+            <div>
+                <h2 class="text-white
+                text-2xl font-bold">{{auth()->user()->name}}</h2>
+                <p class="text-gray-400">{{auth()->user()->email}}</p>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
