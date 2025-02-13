caf71b27d38c12611b8cae09f27c326550431401
ifnuu01 <ifnuu01@gmail.com>
2025-02-13 20:58:47 +0800

    perbaiki bug ui

--- resources/views/show-film.blade.php
+++ resources/views/show-film.blade.php
@@ -10,12 +10,24 @@
     
     @include('layouts.navbar-user')
 
-    <main class="px-4 lg:px-10 grid grid-cols-1 lg:grid-cols-4 gap-4 mt-[167px]">
+    <section class="px-4 lg:px-10 grid grid-cols-1 lg:grid-cols-4 gap-4 mt-[167px]">
         <div class="lg:col-span-3 flex flex-col gap-4">
             <div class="rounded-lg">
                 <img src="{{asset('assets/images/image-2.jpg')}}" class="w-full h-[200px] lg:h-[400px] object-cover rounded-lg">
             </div>
-            <form action="" method="get" class="flex flex-col gap-4 bg-[#22252F] p-4 rounded-lg">
+        </div>
+
+        <div>
+            <div class="bg-[#22252F] p-4 rounded-lg">
+                <h1 class="text-white text-xl font-semibold">Film Title</h1>
+                <h3 class="text-sm text-[#5C636F]">Genre <span class="ml-4">2025-08-25 1:59 PM</span></h3>
+                <p class="text-base text-white mt-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum animi, asperiores odio porro error unde alias nostrum esse quasi magnam voluptatibus, a aliquid quas. Nesciunt natus pariatur magnam nostrum atque.Quos ullam odio fugiat libero numquam dolor magni praesentium, corrupti fuga natus doloribus, eveniet tempora deserunt atque! Aspernatur libero quis neque consequatur quam pariatur tenetur voluptas harum, voluptatem magnam enim.
+                </p>
+            </div>
+        </section>
+        <section class="px-4 lg:px-10 grid grid-cols-1 lg:grid-cols-4 gap-4 mt-4">
+            <form action="" method="get" class="flex flex-col gap-4 bg-[#22252F] col-span-3 p-4 rounded-lg">
                 <div class="flex flex-col gap-4">
                     <label for="rating" class="text-white text-xl font-semibold">Rating</label>
                     <div class="flex items-center gap-2">
@@ -33,50 +45,44 @@
                 </div>
             </form>
 
-            <div class="bg-[#22252F] p-4 rounded-lg">
-                <div class="flex items-center justify-between">
-                    <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
-                    <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+            <div class="col-span-3 flex flex-col gap-y-4">
+                <div class="bg-[#22252F] p-4 rounded-lg">
+                    <div class="flex items-center justify-between">
+                        <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
+                        <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+                    </div>
+                    <p class="text-white text-base mt-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
+                        Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
+                        Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
+                    </p>
                 </div>
-                <p class="text-white text-base mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
-                    Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
-                    Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
-                </p>
-            </div>
-            <div class="bg-[#22252F] p-4 rounded-lg">
-                <div class="flex items-center justify-between">
-                    <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
-                    <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+                <div class="bg-[#22252F] p-4 rounded-lg">
+                    <div class="flex items-center justify-between">
+                        <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
+                        <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+                    </div>
+                    <p class="text-white text-base mt-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
+                        Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
+                        Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
+                    </p>
                 </div>
-                <p class="text-white text-base mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
-                    Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
-                    Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
-                </p>
-            </div>
-            <div class="bg-[#22252F] p-4 rounded-lg">
-                <div class="flex items-center justify-between">
-                    <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
-                    <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+                <div class="bg-[#22252F] p-4 rounded-lg">
+                    <div class="flex items-center justify-between">
+                        <h2 class="text-white text-xl font-semibold"><img src="{{asset('assets/images/image-1.jpg')}}" class="inline mr-4 object-cover w-10 h-10 rounded-full" alt="">User</h2>
+                        <span class="text-sm text-[#5C636F]">2025-08-25 1:59 PM</span>
+                    </div>
+                    <p class="text-white text-base mt-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
+                        Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
+                        Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
+                    </p>
                 </div>
-                <p class="text-white text-base mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, fuga voluptatem a reiciendis asperiores quia consequatur ab modi, autem hic porro non nemo esse saepe doloribus ratione minus! Perferendis, ullam?
-                    Officiis quas magnam, facere ducimus inventore quisquam sequi perferendis et, at placeat praesentium laboriosam molestias? Molestias error fugit eos expedita veniam accusantium! Veniam quis voluptas eaque minus officiis, hic ipsam.
-                    Quasi sed sint minus. Temporibus facilis sequi beatae, adipisci sint sunt, placeat cupiditate eius dolorum ratione tenetur veniam aut voluptate sapiente. Quisquam suscipit, qui iusto veritatis ipsam molestias necessitatibus facere!
-                </p>
             </div>
-        </div>
+        </section>
         
-        <div>
-            <div class="bg-[#22252F] p-4 rounded-lg">
-                <h1 class="text-white text-xl font-semibold">Film Title</h1>
-                <h3 class="text-sm text-[#5C636F]">Genre <span class="ml-4">2025-08-25 1:59 PM</span></h3>
-                <p class="text-base text-white mt-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum animi, asperiores odio porro error unde alias nostrum esse quasi magnam voluptatibus, a aliquid quas. Nesciunt natus pariatur magnam nostrum atque.Quos ullam odio fugiat libero numquam dolor magni praesentium, corrupti fuga natus doloribus, eveniet tempora deserunt atque! Aspernatur libero quis neque consequatur quam pariatur tenetur voluptas harum, voluptatem magnam enim.
-                </p>
-        </div>
-    </main>
+        
 
     @include('layouts.footer-user')
     <script>
