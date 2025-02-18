d00e4070476fe6fc60ee6151e52945c23b055011
ifnuu01 <ifnuu01@gmail.com>
2025-02-18 20:28:13 +0800

    feat : menambahkan route film show & review

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,22 @@ use App\Http\Controllers\CastController;
 use App\Http\Controllers\FilmController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReviewController;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FilmController::class, 'home'])->name('home');
+Route::get('/film/{film}', [FilmController::class, 'show'])->name('film.show');
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
-
+// Route::get('dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
 Route::prefix('admin')->middleware(['auth', 'verified', RoleMiddleware::class . ':admin'])->group(function () {
     Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
     Route::resource('genres', GenreController::class);
-    Route::resource('films', FilmController::class);
+    Route::resource('films', FilmController::class)->except('show');
     Route::resource('casts', CastController::class);
     Route::resource('actors', ActorController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::patch('/profile/photo', [ProfileController::class, 'profileUpdate'])->name('profile.photo.update');
+    Route::post('/film/{film}/review', [ReviewController::class, 'store'])->name('review.store');
 });
 
 require __DIR__ . '/auth.php';
